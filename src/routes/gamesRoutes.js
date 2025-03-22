9fc274d218c510aa4067c6dbcb4ea6b0e1cff2de
Nitesh Kesarkar <niteshkesarkar136@gmail.com>
2025-03-22 11:57:10 +0530

    Screenshots and Movies routes

--- src/routes/gamesRoutes.js
+++ src/routes/gamesRoutes.js
@@ -40,11 +40,11 @@ export default function setupGamesRoutes(client) {
     }
   });
 
-  // Get a Single Game by ID (with Redis caching)
-  router.get(`${BASE_URL}/:id`, async (req, res) => {
+  // Get a Game Movies by Slug (with Redis caching)
+  router.get(`${BASE_URL}/:slug/movies`, async (req, res) => {
     try {
-      const { id } = req.params;
-      const cacheKey = `game:${id}`;
+      const { slug } = req.params;
+      const cacheKey = `game:${slug}:movies`;
 
       // Check Redis for cached data
       const cachedData = await client.get(cacheKey);
@@ -54,7 +54,71 @@ export default function setupGamesRoutes(client) {
       }
 
       // Fetch from API if not in cache
-      const game = await gamesClient.get(id);
+      const game = await gamesClient.get(slug + "/movies");
+
+      // Store in Redis (cache for 24 hours)
+      await client.setEx(
+        cacheKey,
+        process.env.REDIS_CACHE_EXPIRATION || REDIS_CACHE_EXPIRATION,
+        JSON.stringify(game)
+      );
+
+      res.json(game);
+    } catch (error) {
+      res.status(500).json({
+        error: "Error fetching game movie details",
+        details: error.message,
+      });
+    }
+  });
+
+  // Get a Game Screenshots by Slug (with Redis caching)
+  router.get(`${BASE_URL}/:slug/screenshots`, async (req, res) => {
+    try {
+      const { slug } = req.params;
+      const cacheKey = `game:${slug}:screenshots`;
+
+      // Check Redis for cached data
+      const cachedData = await client.get(cacheKey);
+      if (cachedData) {
+        console.log("✅ Serving from Redis Cache");
+        return res.json(JSON.parse(cachedData));
+      }
+
+      // Fetch from API if not in cache
+      const game = await gamesClient.get(slug + "/screenshots");
+
+      // Store in Redis (cache for 24 hours)
+      await client.setEx(
+        cacheKey,
+        process.env.REDIS_CACHE_EXPIRATION || REDIS_CACHE_EXPIRATION,
+        JSON.stringify(game)
+      );
+
+      res.json(game);
+    } catch (error) {
+      res.status(500).json({
+        error: "Error fetching game movie details",
+        details: error.message,
+      });
+    }
+  });
+
+  // Get a Single Game by Slug (with Redis caching)
+  router.get(`${BASE_URL}/:slug`, async (req, res) => {
+    try {
+      const { slug } = req.params;
+      const cacheKey = `game:${slug}`;
+
+      // Check Redis for cached data
+      const cachedData = await client.get(cacheKey);
+      if (cachedData) {
+        console.log("✅ Serving from Redis Cache");
+        return res.json(JSON.parse(cachedData));
+      }
+
+      // Fetch from API if not in cache
+      const game = await gamesClient.get(slug);
 
       // Store in Redis (cache for 24 hours)
       await client.setEx(
